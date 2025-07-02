ad61ba996765a19d2a36573b43524656e81903a2
Sathvika <sathvikaare27@gmail.com>
2025-07-02 21:31:45 +0530

    added new methods

--- Program.cs
+++ Program.cs
@@ -2,11 +2,37 @@
 using LibraryManagementSystem.Services;
 
 Console.WriteLine("Hello, World!");
-//string name=Console.ReadLine();
-//string email=Console.ReadLine();
-//var author = new AuthorService();
-//author.AddAuthor();
+var author = new AuthorService();
 var book=new BookService();
-//book.AddBook();
-int id=Convert.ToInt32(Console.ReadLine());
-book.RemoveBook(id);
+
+Console.WriteLine("Choose an option:");
+Console.WriteLine("1. Add Author");
+Console.WriteLine("2. Add Book");
+Console.WriteLine("3. Get books with Author");
+Console.WriteLine("4. Update Book");
+Console.WriteLine("5.  Remove Book");
+Console.Write("Enter your choice (1-5): ");
+var choice = Convert.ToInt32(Console.ReadLine());
+    switch (choice)
+    {
+        case 1:
+            author.AddAuthor(); 
+            break;
+        case 2:
+            book.AddBook();
+            break;
+        case 3:
+            book.GetBooksWithAuthor();
+            break;
+        case 4:
+            book.UpdateBookbyTitle();
+            break;
+        case 5:
+            book.RemoveBook();
+            break;
+        default:
+            Console.WriteLine("Invalid choice.");
+            break;
+    }
+
+Console.ReadKey();
--- Services/BookService.cs
+++ Services/BookService.cs
@@ -6,19 +6,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace LibraryManagementSystem.Services
 {
     public class BookService
     {
-        public void AddBook()
+        public void AddBook()//1. add book
         {
             try
             {
                 using (var context = new AppDbContext())
                 {
-                    //Console.WriteLine("enetr Book Id");
-                    //int BookId = Convert.ToInt32(Console.ReadLine());
+
                     Console.WriteLine("enetr Book Title ");
                     string title = Console.ReadLine();
                     Console.WriteLine("enetr Book yearpublished");
@@ -36,12 +36,68 @@ namespace LibraryManagementSystem.Services
                 throw;
             }
         }
-        public void RemoveBook(int id)
+        //returns the book and current author
+        public void GetBooksWithAuthor()
+        {
+            try
+            {
+
+                using (var context = new AppDbContext())
+                {
+                    var bookobj = context.Books.Include(a => a.Author).ToList();
+
+                    foreach (var book in bookobj)
+                    {
+                        Console.WriteLine($"BookId: {book.BookId}, Title: {book.Title}, YearPublished: {book.YearPublished}, AuthorName:{book.Author.Name}");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        //update book title
+        public void UpdateBookbyTitle()
+        {
+            try
+            {
+
+                using (var context = new AppDbContext())
+                {
+                    Console.WriteLine("enter the id to change the title");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("enter the new title to change");
+                    string title = Console.ReadLine();
+                    var bookobj = context.Books.Find(id);
+                    if (bookobj != null)
+                    {
+                        bookobj.Title = title;
+                        context.SaveChanges();
+                        Console.WriteLine("book title updated");
+                    }
+                    else
+                    {
+                        Console.WriteLine("book not found");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        //remove book
+        public void RemoveBook()
         {
             try
             {
                 using (var context = new AppDbContext())
                 {
+                    Console.WriteLine("enter the book id to remove the book");
+                    int id=Convert.ToInt32(Console.ReadLine());
                     var bookobj = context.Books.Find(id);
                     if (bookobj != null) {
                         context.Books.Remove(bookobj);
@@ -52,26 +108,6 @@ namespace LibraryManagementSystem.Services
             catch(Exception ex) 
             {
                     Console.WriteLine($"{ex.Message}");
-            }
-        }
-        public void GetBooksWithAuthor()
-        {
-            try
-            {
-
-                using (var context = new AppDbContext())
-                {
-                    var bookobj = context.Books.Include(a=>a.Author).ToList();
-                    if (bookobj != null)
-                    {
-                        Console.WriteLine(bookobj);
-                    }
-                }
-
-            }
-            catch (Exception ex)
-            {
-
             }
         }
 
